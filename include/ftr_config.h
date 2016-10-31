88213647f10cb60fc5f331fce49475107517c48d
AustinHaugerud <austin15328@hotmail.com>
2016-10-31 08:12:06 -0700

    Minor config fix

--- include/ftr_config.h
+++ include/ftr_config.h
@@ -29,20 +29,14 @@
 #define ftr_config_h
 
 #if defined(_WIN32) || defined(__WIN32__)
-
-    #if defined(FTR_EXPORT)
-        #define FTR_API __declspec(dllexport)
-    #else
-        #define FTR_API __declspec(dllimport)
-    #endif
-
+    #define FTR_API __declspec(dllexport)
 #elif defined(linux) || defined(__linux)
     #define FTR_API
 #endif
 
 #define FTR_VERSION_MAJOR 1
 #define FTR_VERSION_MINOR 2
-#define FTR_VERSION_PATCH 0
+#define FTR_VERSION_PATCH 1
 
 #endif
 
